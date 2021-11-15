6456d9eed55ee7c5483948e86605aff31327e3e5
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-15 18:18:50 +0200

    Init project

---
+++ Ereceipt/Ereceipt.Domain/Models/BaseModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Ereceipt.Domain.Models
+{
+    public class BaseCreateModel
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedFromIP { get; set; }
+    }
+    public class BaseEditModel : BaseCreateModel
+    {
+        public DateTime? LastUpdatedAt { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public string LastUpdatedFromIP { get; set; }
+    }
+    public class BaseModel : BaseEditModel
+    {
+        public int Version { get; set; }
+    }
+    public class BaseModel<TypeId> : BaseModel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public TypeId Id { get; set; }
+    }
+}

13d1028757d100cdfd66a3078b4f356cb270b90e
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-15 21:57:35 +0200

    Added base controller

---
+++ Ereceipt/Ereceipt.Web/Controllers/V1/IdentityController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ereceipt.Web.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    public class IdentityController : ApiBaseController
+    {
+
+    }
+}
--- Ereceipt/Ereceipt.Web/Program.cs
+++ Ereceipt/Ereceipt.Web/Program.cs
@@ -1,7 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseApiVersioning();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();

744e0600d03c59b5ecc5bfbdc7bc12d8b7a51325
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-15 21:53:23 +0200

    Added other projects

---
+++ Ereceipt/Ereceipt.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ereceipt.Infrastructure.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddUSNServices(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+    }
+}
---
+++ Ereceipt/USN.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace USN.Infrastructure.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddEreceiptServices(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+    }
+}
